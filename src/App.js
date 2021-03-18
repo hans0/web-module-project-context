e20b7ad11f3e420ecc115e28c3dfb841c41312ec
hans0 <kiessler.hans@gmail.com>
2021-03-18 17:08:16 -0400

    Initial commit, created ProductContext

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import ProductContext from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([
+			...cart, 
+			item
+		])
 	};
 
 	return (
